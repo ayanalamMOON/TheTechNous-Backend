8d16dfd3d86820fafec5ece722af87c8cd8739c0
Ayan Alam <83778128+ayanalamMOON@users.noreply.github.com>
2024-09-07 23:35:39 +0530

    Changes committed: 	new file:   .gitignore 	new file:   Backend/api/api/__init__.py 	new file:   Backend/api/api/asgi.py 	new file:   Backend/api/api/settings.py 	new file:   Backend/api/api/urls.py 	new file:   Backend/api/api/wsgi.py 	new file:   Backend/api/manage.py 	new file:   Backend/app/__init__.py 	new file:   Backend/app/models.py 	new file:   Backend/app/routes/__init__.py 	new file:   Backend/app/routes/admin.py 	new file:   Backend/app/routes/auth.py 	new file:   Backend/app/routes/blog.py 	new file:   Backend/config.py 	new file:   Backend/requirements.txt 	new file:   Backend/run.py

---
+++ Backend/app/models.py
@@ -0,0 +1,27 @@
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Interger, primary_key=True)
+    username = db.Column(db.String(64), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db,Column(db.Boolean, nullable=false)
+    is_admin = db.Column(db.Boolean, default=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    author_id = db.column(db.Integer, db.ForeignKey('user'.id), nullable= False)
+    created_at = db.column(db.DateTime, server_default=db.func.now())
+    updated_at = db.column(db.DateTime, server_default=db.func.now(), onupdate=db.func.now())
+
+    author = db.relationship('User', backref=db.backref('blog_posts', lazy=true))
---
+++ Backend/app/routes/auth.py
@@ -0,0 +1,40 @@
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login', methods=['POST'])
+def register():
+    data = tequest.get_json()
+    username = data.get('username')
+    email = data.get('email')
+    password = data.get('password')
+
+    if User.query.filter_by(username=username).first():
+        return jsonify({'message': 'Username already exists'}), 400
+    
+    if User.query.filter_by(email=email).first():
+        return jsonify({'message': 'Email already exists}), 400
+
+
+    new_user = User(username=username, email=email)
+    new_user.set_password(password)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({'message': 'User created successfully'}), 201
+
+@auth.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    user = User.query.filter_by(username=data.get('username')).first()
+
+    if user and user,check_password(data.get('password')):
+        access_token = create_access_token(identity=user.id)
+        return jsonify({'access_token': access_token}), 200
+
+    return jsonify({'message': 'Invalid login credentials'}), 401
+
+@auth.route('/profile', methods=['GET'])
+@jwt_required()
+def profile():
+    current_user = get_jwt_identity()
+    user = User.query.get(current_user)
+    return jsonify({'username': user.username, 'email': user.email, is_admin=user.is_admin}), 200
