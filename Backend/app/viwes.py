60a541a14fc7d6d3a011491a3b1cbc2f1fc58d5c
Ayan Alam <83778128+ayanalamMOON@users.noreply.github.com>
2024-09-13 15:09:01 +0530

    modified:   Backend/app/viwes.py

--- Backend/app/viwes.py
+++ Backend/app/viwes.py
@@ -3,3 +3,17 @@ from routes.admon import admin
 from routes.blog import blog
 from routes.auth import auth
 from models import db
+
+app = Flask(__name__)
+
+app.config['SQLALCHAMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+app.register_blueprint(admin, url_prefic='/admin')
+app.register_blueprint(blog, url_prefix='/blog')
+app.register_blueprint(auth, url_prefix='/auth')
+
+with app.app_context():
+    db.create_all()
